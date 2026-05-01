f4210e19221ca2dbe78b7148b9522fe12f632fd6
Bruno do Nascimento Maciel <brunodonascimentomaciel@gmail.com>
2026-05-01 03:01:47 -0300

    removing schema validation

--- src/guarita/diplomat/http_server.clj
+++ src/guarita/diplomat/http_server.clj
@@ -1,7 +1,6 @@
 (ns guarita.diplomat.http-server
   (:require [guarita.diplomat.http-server.fraud-score :as diplomat.http-server.fraud-score]
             [guarita.diplomat.http-server.ready :as diplomat.http-server.ready]
-            [guarita.wire.in.fraud-score :as wire.in.fraud-score]
             [io.pedestal.service.interceptors :as pedestal.service.interceptors]
             [service.interceptors]))
 
@@ -10,6 +9,6 @@
     :route-name :ready]
    ["/fraud-score"
     :post [pedestal.service.interceptors/json-body
-           (service.interceptors/wire-in-body-schema wire.in.fraud-score/FraudScore)
+           #_(service.interceptors/wire-in-body-schema wire.in.fraud-score/FraudScore)
            diplomat.http-server.fraud-score/fraud-score!]
     :route-name :fraud-score]])
